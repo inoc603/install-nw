d82bdf57602230086ba883fb1b66de64580574ec
Eddie Huang <inoc603@gmail.com>
2015-06-13 15:13:29 +0900

    fix minor spelling mistake which caused download failure on windows
    'win32' is misspelled as 'win3', and it makes wrong download link on windows. This simple fix should get things working.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var progress = require('progress-stream');
 var semver = require('semver');
 
 var platform = process.platform === 'darwin' ? 'osx' : 
-  process.platform === 'win3' ? 'win' : process.platform;
+  process.platform === 'win32' ? 'win' : process.platform;
 
 var ext = platform === 'linux' ? '.tar.gz' : '.zip';
 
